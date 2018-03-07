b4516870b5eb139e45e227fce6a56624346e1594
jamie <jamie.zhang1003@gmail.com>
2018-03-07 16:35:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Xello - Web Developer Assignment
+
+This project is using Drupal to create one webpage according to the template here: https://careercruising.invisionapp.com/share/2DFYTZ5SKJ5#/screens/280629266_xello_dev_Sample 
+
+
+## Installing
+
+- Download the `xello folder`
+
+- Upload it to your web server
+
+## Running the tests
+
+- If you are using a live online server, please go to your route then add /xello
+
+- If you are using web server software like XAMPP, please run it and type localhost/xello on your browser
+
+
+## About SEO
+
+I have installed Yoast SEO extension in order to add meta tags for the page. For the optimization in the furture, Schema Markup and Sitemap. Add/link google reviews or other trustful social media accounts is a good idea to keep this page up to date. Build backlinks as much as possible to this site will also be helpful to the SEO.
---
+++ sites/default/files/php/twig/5a9f1130e4ab9_menu-local-task.html.twig_s52UqFhlaw-XhawIL8fswTtnX/EsNwvgGnaNaPV3Dzp_-4myeP7V31Byh81TOGM0dA8_g.php
@@ -0,0 +1,81 @@
+<?php
+
+/* themes/bootstrap/templates/menu/menu-local-task.html.twig */
+class __TwigTemplate_c066c53b919b8f1d75352dc6e6b9ac4a2b8109272904a81b83ada147777ccf16 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 19);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 19
+        $context["classes"] = array(0 => ((        // line 20
+($context["is_active"] ?? null)) ? ("active") : ("")));
+        // line 22
+        echo "<li";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["attributes"] ?? null), "addClass", array(0 => ($context["classes"] ?? null)), "method"), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["link"] ?? null), "html", null, true));
+        echo "</li>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/bootstrap/templates/menu/menu-local-task.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  46 => 22,  44 => 20,  43 => 19,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/bootstrap/templates/menu/menu-local-task.html.twig", "C:\\Users\\ward\\Documents\\Software\\xampp\\htdocs\\xello\\themes\\bootstrap\\templates\\menu\\menu-local-task.html.twig");
+    }
+}
